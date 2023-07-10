fd6d6863c890a11b00d6986f51b06b8151da8d89
Lukasz Jurkowski <jjura@windowslive.com>
2023-07-10 07:31:35 +0000

    Initial commit.

---
+++ home/.config/nvim/init.lua
@@ -0,0 +1,9 @@
+-- init.lua
+
+require "core.config"
+require "core.keymap"
+
+require "plain-color"
+require "plain-line"
+require "plain-language".configure("c")
+require "plain-language".configure("cpp")
