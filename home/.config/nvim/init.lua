d0e10294c11ea92de0252f0cfe703fe64e8bb53b
Lukasz Jurkowski <jjura@windowslive.com>
2025-04-10 15:24:51 +0000

    Initial commit.

---
+++ home/.config/nvim/init.lua
@@ -0,0 +1,59 @@
+--------------------------------------------------------------------------------
+-- Command:
+--------------------------------------------------------------------------------
+vim.cmd [[ colorscheme vim ]]
+
+--------------------------------------------------------------------------------
+-- Diagnostic:
+--------------------------------------------------------------------------------
+local config = {
+    virtual_text = true
+}
+
+vim.diagnostic.config(config)
+
+--------------------------------------------------------------------------------
+-- Global:
+--------------------------------------------------------------------------------
+vim.g.mapleader           = " "
+vim.g.netrw_banner        = false
+vim.g.netrw_liststyle     = 3
+vim.g.netrw_sort_sequence = "[/]$"
+vim.g.netrw_winsize       = -40
+
+--------------------------------------------------------------------------------
+-- Option:
+--------------------------------------------------------------------------------
+vim.opt.colorcolumn    = "80"
+vim.opt.completeopt    = "menuone,noinsert,popup"
+vim.opt.expandtab      = true
+vim.opt.ignorecase     = true
+vim.opt.number         = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth     = 4
+vim.opt.showcmd        = false
+vim.opt.tabstop        = 4
+vim.opt.wrap           = false
+
+--------------------------------------------------------------------------------
+-- Keymap:
+--------------------------------------------------------------------------------
+local options = {
+    noremap   = true,
+    silent    = false,
+}
+
+vim.keymap.set("i", "<C-Space>", "<C-x><C-o>", options)
+vim.keymap.set("n", "<Leader>e", ":e **/*", options)
+vim.keymap.set("n", "<Leader>q", ":Lexplore<CR>", options)
+
+--------------------------------------------------------------------------------
+-- Language:
+--------------------------------------------------------------------------------
+vim.lsp.config.clangd = {
+    cmd               = { "clangd" },
+    filetypes         = { "c", "cpp" },
+    root_markers      = { "compile_commands.json" },
+}
+
+vim.lsp.enable("clangd")
